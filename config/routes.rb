1b58e6a5fd8dc5a58bee0f798963100dc23debf2
ar00gula <arianna.mayer@gmail.com>
2020-11-30 17:21:49 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :authors, except: [:destroy, :index]
+  resources :posts, except: [:destroy, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
